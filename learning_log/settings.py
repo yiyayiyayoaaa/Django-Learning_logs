4cab132d28a18ee9c86610a9f66838526bc26c09
cheng.xiao <18272175681@163.com>
2017-06-14 11:25:36 +0800

    commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -144,6 +144,6 @@ if os.getcwd() == '/app':
     #静态资源配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = {
-        os.path.join(BASE_DIR, 'static')
-    }
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
